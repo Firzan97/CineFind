c8fb9d94867f7e73af253bbbc928b5ba42f4efa8
Hairul97 <HairulNajmi77@gmail.com>
2019-12-12 15:51:58 +0800

    redirect when user login

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -63,7 +63,7 @@ class AppController extends Controller
     }
     public function beforeFilter(Event $event)
     {
-        $this->Auth->allow(['index', 'view','adminlogin']);
+        $this->Auth->allow(['index', 'view','adminlogin','add']);
     }
     public function beforeRender(Event $event)
     {
